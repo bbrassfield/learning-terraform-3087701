efc8e0189fd004025391a6764e8e56b65748685c
Bill Brassfield <91522180+bbrassfield@users.noreply.github.com>
2024-08-10 12:14:46 -0700

    Change blog to billtest in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "instance_ami" {
-  value = aws_instance.blog.ami
+  value = aws_instance.billtest.ami
 }
 
 output "instance_arn" {
-  value = aws_instance.blog.arn
+  value = aws_instance.billtest.arn
 }
 
 output "instance_public_ip" {
-  value = aws_instance.blog.public_ip
+  value = aws_instance.billtest.public_ip
 }
