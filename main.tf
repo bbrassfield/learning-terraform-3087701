c7e6bccd332c0b83b6d5c5b9adaa1221914a41d0
Bill Brassfield <91522180+bbrassfield@users.noreply.github.com>
2024-08-21 17:16:07 -0700

    Use more powerful EC2 instance t2.2xlarge, with 8 CPU cores and 32G of RAM. Install the idmc_secure_agent_installer.py script in the home directory of informatica.

--- main.tf
+++ main.tf
@@ -37,11 +37,13 @@ resource "aws_instance" "billtest" {
   user_data = <<-EOF
               #!/bin/bash
               apt update
-              apt install -y joe parted zfs-fuse command-not-found
+              apt install -y joe parted command-not-found
               apt update
               groupadd -g 40034 informatica
               useradd -u 40034 -g informatica informatica
               cp -rp /etc/skel /home/informatica
+              mkdir /home/informatica/install
+              chmod 755 /home/informatica/install
               chown -R informatica.informatica /home/informatica
               mkdir /mnt/informatica
               chown informatica.informatica /mnt/informatica
@@ -86,6 +88,34 @@ resource "aws_instance" "billtest" {
               echo "informatica soft nofile 380000" >> /etc/security/limits.conf
               echo "" >> /etc/security/limits.conf
               echo "# End of file" >> /etc/security/limits.conf
+              echo "H4sIAJqBxmYAA61W227cNhB9369g94USqpWaoAjaBfRgxA6wQBIEvqAPaSAw0qyW" >> /home/informatica/z.b64
+              echo "sUSqvGTtGAb6G/29fkmHpK6O6yZAFwYskjNzZs5cSN52UhnCVN0xpWHFw1rBHxa0" >> /home/informatica/z.b64
+              echo "0cNajl+ftBTDt+EtrFYV7ImG0iooWA3CFFxow5omshqUYC0kpGNaH6WqElLJo2gk" >> /home/informatica/z.b64
+              echo "q4qKq5zShPTC4zqYqJW0Xf5WCoi3K4K/RtZcFFY1JCf0YEynt1lWtRurUy72UrXM" >> /home/informatica/z.b64
+              echo "8JKVjbRVWso2a1nGOp59fp45LzKvTr0l3C4UaGlVCc6YB8x6N0Dt0FrWcGFvXvxM" >> /home/informatica/z.b64
+              echo "VzPsjt06x1Hljg6R0e0YIx1ipNvh695rK9QY6Ew7qU00xpJ4NvOF/TgoFe7Eaabu" >> /home/informatica/z.b64
+              echo "I4pH10FUneTCuDMv9J6iC59BXamGfhjFNGjNpSh4NRPk5UXY3lUz0Z7Uu/uBsbt7" >> /home/informatica/z.b64
+              echo "mgZKozlisqAuXo0GDsAqUJ6YOcJ26ccjbNQQEDwVwYrOJ4tPUTGUjZHXIOYhhv1L" >> /home/informatica/z.b64
+              echo "t93HOKZ2JjaU4URaeYDyWtu2p2MQHLZPv1LY8wZc5lF4REh113AT0YzG7zfPPgSK" >> /home/informatica/z.b64
+              echo "+H5R9SRHsmmoavdbnmGnOVrKYxXFo/qsRx5qL44WtoK2UbeT8JGbw5L90fOEaKOA" >> /home/informatica/z.b64
+              echo "tfmlshATplFOd1JomNR9NvrdVDGuocAyKdCCsbrPywJKdiAiDKhj5pB+whqK5g4m" >> /home/informatica/z.b64
+              echo "I4VxQujxI/Ww+yWeZ1oqTI8V1xjt5AA3oIpSCoPtG/njQvMvkP/y7Nfn8ddGvKH0" >> /home/informatica/z.b64
+              echo "qFAtSE/+dgqrO1q/Qm9GBqEi2pYlVu/eNs3tD+sgDjcldNN4TMMaC1yn52HvbNhx" >> /home/informatica/z.b64
+              echo "wczI60HOlMJoBhQuamIO4InYrhMydBVyVh6QogVf05m+1QbaCIuzlRX58YbMOnbk" >> /home/informatica/z.b64
+              echo "NH4onWJjkw0n2kkYsjm9ujg7L3ZvLy5PXr8uTnfn+SNmFgM6fujeIrczwYSyrtOZ" >> /home/informatica/z.b64
+              echo "H62lVEAf8wbnNvMSWHpMGdvRIORulVQ3AF304qcesWfvpRR7Xltc1OupPWZXxsT3" >> /home/informatica/z.b64
+              echo "AgrLRMsGTpzgGyZQAVv1QMreHPhx8RuW7LkVDh4JDX8jH9Ndtpg8i/uqDxKaedN8" >> /home/informatica/z.b64
+              echo "vx/fhhz/KzNoru0aMOAoQnqKwqkXhRsd66JoGV42xTp46C99NzmGB0B6omrbonvv" >> /home/informatica/z.b64
+              echo "/Ek0q19Z53R3+ublLrhBk2mEgS4V91WPIuO4dX27my5ncuEfCcRHr/tkBwdSVlUF" >> /home/informatica/z.b64
+              echo "65GjcTrPIA7QdDl9LUvme8vIsYd8A4X3R0jGU5b54Dt5wnIv9F2GNxZt0s3GZQu/" >> /home/informatica/z.b64
+              echo "3IDgCio/Th9GceUo91eH3HsQpzSgDiPhm4G7AOwfHdV/Qb/rnyb/C3IdkH3tz4Ff" >> /home/informatica/z.b64
+              echo "MeyAh8g7UTa2AnI8YIXfSkuODDsf2UajHjaMArchcMxybZw/85IhHiilv4+W6c5F" >> /home/informatica/z.b64
+              echo "wTVeND53XIu///zLYEAeq3L3hTONDdEyga/PCefIMcEfwUlwgwaO4lGs4ZWjao1N" >> /home/informatica/z.b64
+              echo "0tPg/zki9HBHP/r8dQKpIzjx+mnIUL8Yqrdf9jr9KkyT1eofoje29p0LAAA=" >> /home/informatica/z.b64
+              cat /home/informatica/z.b64 | openssl base64 -d | gunzip > /home/informatica/idmc_secure_agent_installer.py
+              chown informatica.informatica /home/informatica/idmc_secure_agent_installer.py
+              chmod 700 /home/informatica/idmc_secure_agent_installer.py
+              rm -f /home/informatica/z.b64
               parted /dev/nvme1n1 mklabel gpt
               parted /dev/nvme1n1 mkpart primary ext4 0% 100%
               mkfs.ext4 /dev/nvme1n1p1
