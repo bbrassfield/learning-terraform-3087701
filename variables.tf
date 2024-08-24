8b966909ace57ccdd04a26c1c14aa783e617c59e
Bill Brassfield <91522180+bbrassfield@users.noreply.github.com>
2024-08-24 14:07:29 -0700

    Declared 3 new variables with same names as the env variables set in the terraform.io web UI.

--- variables.tf
+++ variables.tf
@@ -2,3 +2,24 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t2.2xlarge"
 }
+
+variable "idmc_sa_installer_user" {
+  description = "Username passed into the idmc_secure_agent_installer.py script"
+  type        = string
+  sensitive   = true
+  default     = "joe_user"
+}
+
+variable "idmc_sa_installer_pass" {
+  description = "Password passed into the idmc_secure_agent_installer.py script"
+  type        = string
+  sensitive   = true
+  default     = "joe_pass"
+}
+
+variable "idmc_sa_installer_group" {
+  description = "Group passed into the idmc_secure_agent_installer.py script"
+  type        = string
+  sensitive   = true
+  default     = "joe_group"
+}
