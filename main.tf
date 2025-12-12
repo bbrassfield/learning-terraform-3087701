124fb0b0c7e659aaf0f56da68c3a51dee1b03199
Bill Brassfield <bbrassfield@squaretrade.com>
2025-12-12 13:21:44 -0800

    Removing a couple of no-longer-needed data resources.

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
+# data "aws_ami" "app_ami" {
+#   most_recent = true
+#
+#   filter {
+#     name   = "name"
+#     values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+#   }
+#
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+#
+#   owners = ["979382823631"] # Bitnami
+# }
+#
+# data "aws_vpc" "default" {
+#   default = true
+# }
 
 # resource "aws_instance" "billtest" {
 #   ami           = "ami-0075013580f6322a1"
