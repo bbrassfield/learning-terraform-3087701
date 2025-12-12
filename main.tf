a6e0613b06aeff0ffe9fa75f8e13a0949f1ab9ac
Bill Brassfield <bbrassfield@squaretrade.com>
2025-12-12 13:13:31 -0800

    Removing a no-longer-needed security group.

--- main.tf
+++ main.tf
@@ -162,52 +162,52 @@ data "aws_vpc" "default" {
 #               EOF
 # }
 
-resource "aws_security_group" "billtest" {
-  name        = "billtest"
-  description = "Allow https and https in, Allow everything out"
-
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "billtest_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.billtest.id
-}
-
-resource "aws_security_group_rule" "billtest_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.billtest.id
-}
-
-resource "aws_security_group_rule" "billtest_ssh_in" {
-  type        = "ingress"
-  from_port   = 22
-  to_port     = 22
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.billtest.id
-}
-
-resource "aws_security_group_rule" "billtest_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.billtest.id
-}
+# resource "aws_security_group" "billtest" {
+#   name        = "billtest"
+#   description = "Allow https and https in, Allow everything out"
+#
+#   vpc_id = data.aws_vpc.default.id
+# }
+#
+# resource "aws_security_group_rule" "billtest_http_in" {
+#   type        = "ingress"
+#   from_port   = 80
+#   to_port     = 80
+#   protocol    = "tcp"
+#   cidr_blocks = ["0.0.0.0/0"]
+#
+#   security_group_id = aws_security_group.billtest.id
+# }
+#
+# resource "aws_security_group_rule" "billtest_https_in" {
+#   type        = "ingress"
+#   from_port   = 443
+#   to_port     = 443
+#   protocol    = "tcp"
+#   cidr_blocks = ["0.0.0.0/0"]
+#
+#   security_group_id = aws_security_group.billtest.id
+# }
+#
+# resource "aws_security_group_rule" "billtest_ssh_in" {
+#   type        = "ingress"
+#   from_port   = 22
+#   to_port     = 22
+#   protocol    = "tcp"
+#   cidr_blocks = ["0.0.0.0/0"]
+#
+#   security_group_id = aws_security_group.billtest.id
+# }
+#
+# resource "aws_security_group_rule" "billtest_everything_out" {
+#   type        = "egress"
+#   from_port   = 0
+#   to_port     = 0
+#   protocol    = "-1"
+#   cidr_blocks = ["0.0.0.0/0"]
+#
+#   security_group_id = aws_security_group.billtest.id
+# }
 
 # resource "aws_ebs_volume" "billtestvol1" {
 #   availability_zone = aws_instance.billtest.availability_zone
